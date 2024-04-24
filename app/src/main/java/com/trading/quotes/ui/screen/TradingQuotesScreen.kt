41dc0ef6f52ecf8570879b5381d8d67ec77fa4bf
m.v.tokarev <m.v.tokarev@tinkoff.ru>
2024-04-24 19:21:13 +0400

    Add ui components for main screen

--- app/src/main/java/com/trading/quotes/ui/screen/TradingQuotesScreen.kt
+++ app/src/main/java/com/trading/quotes/ui/screen/TradingQuotesScreen.kt
@@ -9,10 +9,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,11 +37,13 @@ internal fun TradingQuotesScreen(state: TradingQuotesState, modifier: Modifier =
         contentPadding = PaddingValues(bottom = 8.dp),
     ) {
         items(items = state.quotes, key = { quote -> quote.ticker }) { quote ->
-            TradingQuoteItem(state = quote)
+            TradingQuoteItem(state = quote, modifier = Modifier.padding(all = 8.dp))
             Spacer(
                 modifier = Modifier
                     .height(1.dp)
-                    .background(Color.Gray)
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .background(Color.LightGray)
             )
         }
     }
@@ -49,15 +51,10 @@ internal fun TradingQuotesScreen(state: TradingQuotesState, modifier: Modifier =
 
 @Composable
 internal fun TradingQuoteItem(state: TradingQuoteState, modifier: Modifier = Modifier) {
-    Row(
-        modifier
-            .wrapContentSize()
-            .padding(all = 16.dp)
-    ) {
+    Row(modifier) {
         Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center) {
             Row {
                 AsyncImage(
-                    onError = {},
                     model = state.logo,
                     contentDescription = null
                 )
@@ -66,20 +63,21 @@ internal fun TradingQuoteItem(state: TradingQuoteState, modifier: Modifier = Mod
             Text(
                 text = state.lastTradingPlatform,
                 style = MaterialTheme.typography.labelMedium,
-                color = Color.LightGray,
+                color = Color.Gray,
             )
         }
 
         Column(Modifier, horizontalAlignment = Alignment.End) {
-            Text(text = state.change, color = state.changeColor)
-            Text(text = state.lastPrice, style = MaterialTheme.typography.labelSmall)
+            Text(text = state.change, color = state.changeColor, style = MaterialTheme.typography.titleLarge)
+            Text(text = state.lastPrice, style = MaterialTheme.typography.labelMedium)
         }
         Image(
             painter = painterResource(id = R.drawable.ic_arrow_right),
             contentDescription = null,
+            colorFilter = ColorFilter.tint(Color.Gray),
             modifier = Modifier
                 .align(Alignment.CenterVertically)
-                .padding(start = 8.dp)
+                .padding(start = 4.dp)
         )
     }
 }
